65d065257527c83abdb33d558c70674413c199e8
Jonathan Fritz <fritz@nosc.io>
2022-06-03 14:39:38 +0200

    Now using 15 ai

--- video_creation/voices.py
+++ video_creation/voices.py
@@ -1,9 +1,26 @@
+import os
 from gtts import gTTS
 from pathlib import Path
 from mutagen.mp3 import MP3
+from utils.FifteenApi import FifteenAPI
 from utils.console import print_step, print_substep
 from rich.progress import track
+from num2words import num2words
 
+def create_text_with_fifteen(text, filename):
+    fifteen = FifteenAPI(show_debug = True)
+    character = "The Narrator"
+    fifteen.save_to_file(character, text, filename)
+    
+#Replaces the number in the text with the spoken word.
+#     Args:
+#         text: The text you want to replace the number with the spoken word.
+def replace_number_with_spoken_word(text):
+    for number in text.split():
+        if number.isdigit():
+            print("Replacing " + number)
+            text = text.replace(number, num2words(int(number), lang="en_US"))
+    return text
 
 def save_text_to_mp3(reddit_obj):
     """Saves Text to MP3 files.
@@ -22,12 +39,22 @@ def save_text_to_mp3(reddit_obj):
     length += MP3(f"assets/mp3/title.mp3").info.length
 
     for idx, comment in track(enumerate(reddit_obj["comments"]), "Saving..."):
-        # ! Stop creating mp3 files if the length is greater than 50 seconds. This can be longer, but this is just a good starting point
-        if length > 50:
-            break
-        tts = gTTS(text=comment["comment_body"], lang="en", slow=False)
-        tts.save(f"assets/mp3/{idx}.mp3")
-        length += MP3(f"assets/mp3/{idx}.mp3").info.length
+        if len(comment["comment_body"]) > 199:
+            # ! Stop creating mp3 files if the length is greater than 50 seconds. This can be longer, but this is just a good starting point
+            if length > 50:
+                break
+            tts = gTTS(text=comment["comment_body"], lang="en", slow=False)
+            tts.save(f"assets/mp3/{idx}.mp3")
+            length += MP3(f"assets/mp3/{idx}.mp3").info.length
+        else:
+            if length > 50:
+                break
+            comment_text = comment["comment_body"]
+            comment_text = comment_text.split("https://")[0]
+            comment_text = replace_number_with_spoken_word(comment_text)
+            create_text_with_fifteen(comment_text, f"assets/mp3/{idx}")
+            os.system(f"ffmpeg -i assets/mp3/{idx}.wav assets/mp3/{idx}.mp3")
+            length += MP3(f"assets/mp3/{idx}.mp3").info.length
 
     print_substep("Saved Text to MP3 files successfully.", style="bold green")
     # ! Return the index so we know how many screenshots of comments we need to make.
