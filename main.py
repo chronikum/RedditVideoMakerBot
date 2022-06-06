9e9da9ef5a7e74979cd607981bc343c69f31a350
Jonathan Fritz <fritz@nosc.io>
2022-06-06 17:52:55 +0200

    Now kills the program if it failed too often

--- main.py
+++ main.py
@@ -15,14 +15,19 @@ print_markdown(
 
 time.sleep(3)
 
+kill_incrementer = 0
+
 # we are using exceptions to restart the program.
 # this is really unclean and I know I deserve punishing for it
 # There for sure is an esaier solution, but you can throw exceptions anytime
 # and the program will restart. That is nice! :D
 def get_video_and_render_it():
+    global kill_incrementer
+    if (kill_incrementer == 15):
+        print("Killing the program. Failed too often!")
+        sys.exit(1)
     try:
         reddit_object = get_subreddit_threads()
-
         length, number_of_comments = save_text_to_mp3(reddit_object)
         download_screenshots_of_reddit_posts(reddit_object, number_of_comments)
         download_background()
@@ -32,6 +37,7 @@ def get_video_and_render_it():
         print("\n\nExiting...")
         return sys.exit(0)
     except Exception as e:
+        kill_incrementer+=1
         get_video_and_render_it()
 
 get_video_and_render_it()
